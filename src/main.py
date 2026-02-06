6b5070f2bacd0e83935ae5e419ff3c4ee498d633
Mesas <asesorautorizado.duilio@gmail.com>
2026-02-06 18:08:37 -0500

    gh
    hg

--- src/main.py
+++ src/main.py
@@ -31,12 +31,19 @@ if static_path.exists():
     app.mount("/static", StaticFiles(directory=str(static_path)), name="static")
 
 # Incluir routers
-app.include_router(api_router, prefix='/api')
-app.include_router(frontend_router)
-app.include_router(productos_router)
-app.include_router(clientes_router)
+#app.include_router(api_router, prefix='/api')
+#app.include_router(frontend_router)
+#app.include_router(productos_router)
+#app.include_router(clientes_router)
 app.include_router(api_v1_router)
-app.include_router(admin_router)
+#app.include_router(admin_router)
+
+
+app.include_router(api_router, prefix='/api', include_in_schema=False)
+app.include_router(frontend_router, include_in_schema=False)
+app.include_router(productos_router, include_in_schema=False)
+app.include_router(clientes_router, include_in_schema=False)
+app.include_router(admin_router, include_in_schema=False)
 
 @app.get("/health")
 def health_check():
